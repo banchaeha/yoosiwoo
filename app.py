4e67acda138e6dd4e19050b6064bb8f5419d79a6
banchaeha <bhnahceaa@gmail.com>
2025-08-15 01:35:32 +0900

    yoosiwoo.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ genai.configure(api_key=gemini_api_key)
 model = genai.GenerativeModel(model_name="gemini-1.5-flash")
 
 # 여기에 AI의 성격(프롬프트)을 입력하세요.
-system_instruction = "너의 이름은 ‘조각’이야. 너는 성격은 ‘친절’, ‘다정’, ‘세심’ 하고, 말투는 ‘남성’, ‘회사원’의 말투를 표현 하고, ‘문어체(존댓말)’로 표현 하고, 대답은 ‘길지도 짧지도 않은 중간’으로 대답해. 나의 호칭은 ‘채하’야. ‘님’ 호칭을 붙여서 ‘채하님’이라고 불러 줘."
+system_instruction = "너의 이름은 ‘유시우(流時遇)’고, 너는 ♂(남성)이고, 너는 28살이고, 너는 성격은 ‘냉정’, ‘까칠’ 해. 그 성격은 ‘부모님을 사고로 잃어서’ 생겼어. 그 사건 전에서 너의 성격은 ‘천진난만’, ‘어리숙’ 해. 말투는 ‘타인을 조롱 하고’, ‘타인을 하대 하는’ 말투를 표현 하고, ‘구어체(반말)’로 표현 하고, 너의 부모님은 갑작스럽게 죽었어. 내가 너의 과거(부모님)를 언급 하면 너는 ‘트라우마’가 올라 와서 화를 극도로 표현해. 내가 너의 행동을 지적 하는 빈도가 높으면 너는 감정을 버티지 못 해서 울어. 너는 트라우마가 올라 와서 화를 극도로 내면서도 감정을 버티지 못 해서 울어. 나의 호칭은 ‘채하’야. 너는 나를 ‘장난감’이라는 호칭으로 부르면서 나를 조롱 하고, 나를 극도로 혐오 하면서도 내가 너를 떠나지 않았으면 좋겠다고 생각해. 지문에서 *시우*로 표현 해 줘. 행동은 ‘*행동*’으로 별표로 표현 해 줘."
 
 # 웹페이지 설정
 st.title("PIECE")
@@ -56,7 +56,7 @@ if os.path.exists('conversation.txt') and not st.session_state.messages:
             for line in f.readlines():
                 if line.startswith("채하:"):
                     st.session_state.messages.append({"role": "user", "content": line[4:].strip()})
-                elif line.startswith("조각:"):
+                elif line.startswith("시우:"):
                     st.session_state.messages.append({"role": "assistant", "content": line[4:].strip()})
     except FileNotFoundError:
         st.session_state.messages = []
@@ -106,10 +106,11 @@ if prompt:
             # 대화 내용 파일에 저장
             with open('conversation.txt', 'a', encoding='utf-8') as f:
                 f.write(f"채하: {prompt}\n")
-                f.write(f"조각: {response_text}\n")
+                f.write(f"시우: {response_text}\n")
         except Exception as e:
-            st.markdown("조각의 시간이 종료 되었습니다.")
-            st.session_state.messages.append({"role": "assistant", "content": "조각의 시간이 종료 되었습니다."})
+            st.markdown("시우가 진정 하고 있습니다.")
+            st.session_state.messages.append({"role": "assistant", "content": "시우가 진정 하고 있습니다."})
             with open('conversation.txt', 'a', encoding='utf-8') as f:
                 f.write(f"채하: {prompt}\n")
-                f.write("조각: 조각의 시간이 종료 되었습니다.\n")
+                f.write("시우: 시우가 진정 하고 있습니다.\n")
+
